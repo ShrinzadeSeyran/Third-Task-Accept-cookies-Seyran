9d0ae9d6bcec18d35a2f2249245a0a5345f53656
ShrinzadeSeyran <seyransiri@gmail.com>
2023-01-08 23:19:51 -0800

    first commit

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-var a=confirm("Zehmet olmasa cerezleri qebul et");
-var b;
-if(a==false){
-    alert("Cerezleri qebul etmediniz");
-    open(location,"_self").close();
-}
-else{
-    b=alert("Cerezleri qebul etdiniz")
-}
+alert("Welcome")
